8c7f416f7ddff60c1c3b20c5292fc63e276f3883
Breanna <breanna@breannawebster.com>
2020-11-10 22:42:44 -0500

    commit tree 0406652ac152a8bed7a42400dda10442f90967ab

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,8 @@
 def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
+    speed > 60 || speed < 40
+  end
+  def not_safe?(speed)
+    60 < speed ? true : false || speed < 40 ? true : false
+  end
 
 
